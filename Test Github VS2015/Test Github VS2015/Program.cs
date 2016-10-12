dd712f439783607295209d0f878c46f1f217ae77
Patryk Chęciński <pchecinski@outlook.com>
2016-10-12 09:11:44 +0200

    Cźwiczenie koło

--- Test Github VS2015/Test Github VS2015/Program.cs
+++ Test Github VS2015/Test Github VS2015/Program.cs
@@ -8,10 +8,61 @@ namespace Test_Github_VS2015
 {
     class Program
     {
+        private static Int32 test(Int32 a)
+        {
+            return a + a;
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world!");
+
+            //Console.WriteLine(test(3));
+
+            double r = 3.23;
+            double pole = Math.PI * r * r;
+
+            Console.WriteLine("format wyswietlania {0} {1:f3}", r, pole);
+
+            /*
+             *  { numer_zmiennej : format wyświetlania }
+             */
+
             Console.ReadKey();
+            return;
+
+            //double r;
+            /*
+            string linia;
+
+            linia = Console.ReadLine();
+
+            int a = Convert.ToInt32(linia) + 2;
+
+            Console.WriteLine(a);
+
+            r = Convert.ToDouble(Console.ReadLine());
+
+            string read = Console.ReadLine();
+            double r = Convert.ToDouble(read);
+            */
+
+ 
+
+            /*
+                
+            Console.Write("Podaj długość promienia: ");
+            r = Convert.ToDouble(Console.ReadLine());
+            double pole = Math.PI * r * r;
+            double obwod = 2 * Math.PI * r;
+            Console.WriteLine("Pole koła o promieniu {0} wynosi: {1:f3}.", r, pole);
+
+            Console.WriteLine("Obwód koła o promieniu {0} wynosi: {1:f3}.", r, obwod);
+            Console.ReadKey();            */
+
+            //Console.WriteLine("Hello world!");
+            //Console.ReadKey();
+            Console.ReadKey();
+            return;
         }
     }
 }
