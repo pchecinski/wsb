da7e4d86f1bd3c3d56e9ba35f7080b2c9de14d99
Patryk Chęciński <pchecinski@outlook.com>
2016-10-19 08:30:11 +0200

    program do liczenia równań kwadratowych

--- if try catch/if try catch/Program.cs
+++ if try catch/if try catch/Program.cs
@@ -10,6 +10,37 @@ namespace if_try_catch
     {
         static void Main(string[] args)
         {
+            double A = 0, B = 0, C = 0;
+
+            Console.WriteLine("Podaj A, B oraz C:");
+
+            Console.Write("A: ");
+            A = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("B: ");
+            B = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("C: ");
+            C = Convert.ToDouble(Console.ReadLine());
+
+
+            double delta = B * B - 4 * A * C;
+            Console.WriteLine("Delta wynosi: {0}", delta);
+
+            if (delta > 0)
+            {
+                Console.WriteLine("x1 = {0}\nx2 = {1}", (-B - Math.Sqrt(delta)) / 2 * A, (-B + Math.Sqrt(delta)) / 2 * A);
+            }
+            else if (delta == 0)
+            {
+                Console.WriteLine("x = {0}", -B / 2 * A);
+            }
+            else
+            {
+                Console.WriteLine("brak rozwiązań, delta ujemna");
+            }
+
+            Console.ReadKey();
         }
     }
 }
