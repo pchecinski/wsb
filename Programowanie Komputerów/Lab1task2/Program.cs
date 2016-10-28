fad299003e9dd50cbd09025c5f3f8c426de6ce4c
Patryk Chęciński <pchecinski@outlook.com>
2016-10-28 14:07:48 +0200

    Lab1 all, Lab 2 task1

---
+++ Programowanie Komputerów/Lab1task2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System; // brak using System
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1task2
+{
+    class Program
+    {
+        static void Main(string[] args) // main
+        {
+            /* 
+             Ćwiczenie 1
+                Popraw błędy w kodzie.
+             */
+
+            Console.WriteLine("Zrobił wilk elektrownię, lecz by prąd uzyskać"); // brak "
+            Console.WriteLine("Spalał w niej cały węgiel z kopalni od liska.");
+            Console.WriteLine("Kopalnia z elektrowni cały prąd zżerała, ");      // brak "
+            Console.WriteLine("Stąd brak światła i węgla. Ale system działa!");
+            Console.WriteLine();
+            Console.WriteLine("Andrzej Waligórski"); // brak średnika
+            Console.ReadLine(); // brak Console
+        }
+    }
+}
--- Programowanie Komputerów/Programowanie Komputerów/Program.cs
+++ Programowanie Komputerów/Programowanie Komputerów/Program.cs
@@ -4,12 +4,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Programowanie_Komputerów
+namespace Lab1task1
 {
     class Program
     {
         static void Main(string[] args)
         {
+            /* 
+             Ćwiczenie 1
+                Utworzenie programu, który wypisuje na ekranie powitanie i czeka na reakcje
+                użytkownika. Potem wypisuje "Koniec" i znów czeka na reakcję użytkownika i
+                następnie kończy działanie.
+             */
+            Console.WriteLine("Witam Pana(ią)");
+            Console.ReadKey(true);
+            Console.Write("Koniec");
+
+            Console.ReadKey(true);
         }
     }
 }
